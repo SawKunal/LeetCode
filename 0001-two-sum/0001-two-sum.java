202e252f76a07ab21d6c305bbe0d6d166f8657d9
Kunal <123144000+SawKunal@users.noreply.github.com>
2025-08-25 11:03:03 +0530

    Time: 3 ms (61.89%), Space: 45.6 MB (5.06%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         if (nums[j] == target - nums[i]) {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // // Return an empty vector if no solution is found
+        // return {};
+        Hashtable<Integer, Integer> numbersFound = new Hashtable<>();
+        for(int i=0; i<nums.length; i++){
+            if(numbersFound.containsKey(target - nums[i])){
+                return new int[]{numbersFound.get(target - nums[i]), i};
+            }
+            numbersFound.put(nums[i], i);
+        }
+        return null;
+    }
+}
